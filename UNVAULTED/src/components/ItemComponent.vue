1d67bc87d0ce4c1aeabc7af2ab09f2f08bcf5db3
KptnFishy <nils.miessgang@gmail.com>
2025-10-10 16:28:01 +0200

    updated item component visuals

--- UNVAULTED/src/components/ItemComponent.vue
+++ UNVAULTED/src/components/ItemComponent.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="container flex flex-col w-60 p-2 border border-[var(--color-border)] rounded-xl gap-2">
-    <div class="img-container relative h-72 w-full"> <!-- use a valid height -->
-      <div class="product-overlay absolute inset-0 z-10"> <!-- ensure overlay is on top -->
-        <div class="sell-type absolute top-2 right-2 border border-[var(--color-border)]/40 rounded-full px-1.5 py-0.5 w-fit text-[8px] text-white backdrop-blur-md bg-white/10">
-          {{ sellType }}
+  <div class="container flex flex-col w-60 p-2 border border-[var(--color-border)] rounded-xl gap-2 hover:scale-104 hover:shadow-lg transition duration-200 ">
+    <div class="img-container relative h-72 w-full">
+      <div class="product-overlay absolute inset-0 z-10">
+        <div class="sell-type absolute top-2 right-2 border border-[var(--color-border)]/40 rounded-full px-1.5 py-0.5 w-fit text-[8px] text-white backdrop-blur-md bg-black/20">
+          <span class="mix-blend-difference">{{ sellType }}</span>
         </div>
-        <div class="seller group absolute flex flex-row items-center gap-1 bottom-2 left-2 border border-[var(--color-border)]/40 rounded-full p-[1px] hover:pr-2 w-fit text-xs text-white backdrop-blur-md bg-white/10">
+        <div class="seller group absolute flex flex-row items-center gap-1 bottom-2 left-2 border border-[var(--color-border)]/40 rounded-full p-[1px] hover:pr-2 w-fit max-w-30 text-xs text-white backdrop-blur-md bg-black/20">
           <img :src="sellerImage" class="h-8 w-8 rounded-full aspect-square" />
-          <p class="hidden group-hover:block text-gray-100 text-xs">{{ sellerName }}</p>
+          <p class="hidden group-hover:block text-gray-100 text-xs truncate">{{ sellerName }}</p>
         </div>
-        <div class="like-count absolute bottom-2 right-2 flex flex-row items-center gap-1 px-2 py-1 border border-[var(--color-border)]/40 rounded-full bg-white/10 backdrop-blur-md">
-          <span class="i-lucide-heart size-5"><svg width="16px" height="16px" viewBox="-2 -2 20 16" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" transform="matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.256"></g><g id="SVGRepo_iconCarrier"> <path d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z" fill="#ffffff"></path> </g></svg></span>
+        <div class="like-count absolute bottom-2 right-2 flex flex-row items-center gap-1 px-2 py-1 border border-[var(--color-border)]/40 rounded-full bg-black/20 backdrop-blur-md">
+          <span class="i-lucide-heart size-5"><svg width="16px" height="16px" viewBox="-2 -2 20 16" fill="none" xmlns="http://www.w3.org/2000/svg" :stroke='isLiked ? "#ff0000" : "#ffffff"' transform="matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.256"></g><g id="SVGRepo_iconCarrier"> <path d="M1.24264 8.24264L8 15L14.7574 8.24264C15.553 7.44699 16 6.36786 16 5.24264V5.05234C16 2.8143 14.1857 1 11.9477 1C10.7166 1 9.55233 1.55959 8.78331 2.52086L8 3.5L7.21669 2.52086C6.44767 1.55959 5.28338 1 4.05234 1C1.8143 1 0 2.8143 0 5.05234V5.24264C0 6.36786 0.44699 7.44699 1.24264 8.24264Z" :fill='isLiked ? "#ff0000" : "#ffffff"'></path> </g></svg></span>
           <p class="text-xs text-gray-100">{{ likeCount }}</p>
         </div>
       </div>
@@ -22,9 +22,12 @@
       />
     </div>
 
-    <div class="flex flex-col description-container gap-1">
-      <p class="text-2xl font-bold">{{ title }}</p>
-      <p class="text-md text-gray-600">{{ price }}€</p>
+    <div class="flex flex-col description-container">
+      <span class="text-2xl font-bold line-clamp-2 h-16">{{ title }}</span>
+      <div class="flex flex-row justify-between">
+        <span class="text-md text-gray-600">{{ price }}€</span>
+        <span class="text-md text-gray-600">{{ distance }} km</span>
+      </div>
     </div>
   </div>
 </template>
@@ -56,7 +59,7 @@ const props = defineProps({
         default: 'Trade/Buy'
     },
     sellerImage: {
-        type: URL,
+        type: String,
         required: true,
         default: 'https://tse3.mm.bing.net/th/id/OIP.vcjzhWLCN7-VOC95HeBsJAHaHa?rs=1&pid=ImgDetMain&o=7&rm=3'
     },
@@ -66,6 +69,16 @@ const props = defineProps({
         default: 'Username'
     },
     likeCount: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    isLiked: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    distance: {
         type: Number,
         required: true,
         default: 0
--- UNVAULTED/src/views/HomeView.vue
+++ UNVAULTED/src/views/HomeView.vue
@@ -1,46 +1,43 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import ItemComponent from '../components/ItemComponent.vue';
-import FilterMenu from '../components/FilterMenu.vue'
+import FilterMenu from '../components/FilterMenu.vue';
+
+const items = ref([]);
+
+const getPopularProducts = async () => {
+  const productData = await fetch('https://fakerapi.it/api/v2/products?_quantity=20');
+  const json = await productData.json();
+  return json.data;
+};
+
+const getRandomImage = () => {
+  return `https://picsum.photos/200/300?random=${Math.floor(Math.random()*10000)}`;
+}
+
+onMounted(async () => {
+  items.value = await getPopularProducts();
+});
 </script>
 
 <template>
   <main>
     <div class="h-30"></div>
     <div class="flex flex-row w-full px-[7.5%] gap-5">
-      <FilterMenu></FilterMenu>
+      <FilterMenu />
       <div class="grid justify-end grid-cols-[repeat(auto-fit,minmax(15rem,max-content))] gap-4 w-full">
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
-        <ItemComponent />
+        <ItemComponent
+          v-for="(item, index) in items"
+          :key="index"
+          :title="item.name"
+          :price="item.price"
+          :image="getRandomImage()"
+          :sellerName="item.upc"
+          :likeCount="Math.floor(Math.random() * 1000)"
+          :sellType="Math.round(Math.random()) == 1 ? 'Trade' : 'Trade/Buy'"
+          :is-liked="Math.round(Math.random()) == 1 ? false : true"
+        />
       </div>
     </div>
   </main>
 </template>
-
-<style scoped>
-main{
-  height: 100vh;
-  width: 100vw;
-}
-</style>
